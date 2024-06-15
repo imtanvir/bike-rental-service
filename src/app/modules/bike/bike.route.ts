8f8c894dc3df6cfa8388753e397ba5d481ee1a49
Md. Tanvir Parvej Badhon <tanvirparvej101@gmail.com>
2024-06-15 23:24:55 +0600

    Bike Rental Service Bike Validation

--- src/app/modules/bike/bike.route.ts
+++ src/app/modules/bike/bike.route.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import authCheck from "../../middleware/authCheck";
+import requestValidation from "../../middleware/requestValidation";
 import { USER_ROLE } from "../user/user.constant";
 import { BikeControllers } from "./bike.controller";
+import { BikeValidationSchema } from "./bike.validation";
 
 const router = express.Router();
 
-router.post("/", authCheck(USER_ROLE.admin), BikeControllers.createBike);
+router.post(
+  "/",
+  requestValidation(BikeValidationSchema.bikeValidationSchema),
+  authCheck(USER_ROLE.admin),
+  BikeControllers.createBike
+);
 export const BikesRoutes = router;
